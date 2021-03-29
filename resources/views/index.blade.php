7fa8147cce6873233947a1d93c98fb2fdba4abb7
gabss <gabriellarocca1@outlook.com>
2021-03-29 15:07:51 -0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('js/styles.css') }}"></head>
+        <title>Laravel</title>
+    </head>
+   <body>
+        <app-root></app-root>
+        <script src="{{ asset('js/runtime.js') }}" defer></script>
+        <script src="{{ asset('js/polyfills.js') }}" defer></script>
+        <script src="{{ asset('js/vendor.js') }}" defer></script>
+        <script src="{{ asset('js/main.js') }}" defer></script>
+   </body>
+</html>
